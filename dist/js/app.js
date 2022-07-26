e0fe674ea31625a8c48ad09ee4fd9a2d98c21daf
Yawarzy <yawarbhatzy@gmail.com>
2022-07-26 18:19:30 +0530

    Finished navigation and the hero section for both desktop and mobile

---
+++ dist/js/app.js
@@ -0,0 +1,17 @@
+// Mobile Navigation
+const hamburger = document.querySelector(".hamburger");
+const closeNavBtn = document.querySelector(".close-nav");
+const navList = document.querySelector(".nav__list");
+const overlay = document.querySelector(".overlay");
+
+const openNav = function () {
+  navList.classList.add("toggle-menu");
+  overlay.classList.remove("hidden");
+};
+const closeNav = function () {
+  navList.classList.remove("toggle-menu");
+  overlay.classList.add("hidden");
+};
+
+hamburger.addEventListener("click", openNav);
+closeNavBtn.addEventListener("click", closeNav);
