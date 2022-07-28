78f867fe99538bb2240c54b88dc2b470978b149c
Yawarzy <yawarbhatzy@gmail.com>
2022-07-28 14:00:12 +0530

    Finished success stories section

--- dist/js/app.js
+++ dist/js/app.js
@@ -16,3 +16,26 @@ const closeNav = function () {
 hamburger.addEventListener("click", openNav);
 closeNavBtn.addEventListener("click", closeNav);
 overlay.addEventListener("click", closeNav);
+
+// Success Tabs
+const tabBtns = document.querySelectorAll(".tab-btn");
+const tabs = document.querySelectorAll(".success__tab");
+const tabImage = document.querySelector(".success__image img");
+tabBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    tabBtns.forEach((btn, index) => {
+      btn.classList.remove("btn-primary");
+      btn.classList.add("btn-secondary");
+      tabs[index].classList.remove("hidden");
+    });
+    e.target.classList.remove("btn-secondary");
+    e.target.classList.add("btn-primary");
+    if (e.target.classList.contains("positive")) {
+      tabs[1].classList.add("hidden");
+      tabImage.src = "./images/Success Stories-sucess.png";
+    } else if (e.target.classList.contains("negative")) {
+      tabs[0].classList.add("hidden");
+      tabImage.src = "./images/Success Stories-error.png";
+    }
+  });
+});
